40e7e219e82b80bc616d2a694161b5adfa92a6c5
Kieran Gallagher <kags1995@hotmail.co.uk>
2015-09-22 16:48:31 +0100

    Whoo! first changes

--- include/Common.h
+++ include/Common.h
@@ -2,5 +2,13 @@
 #define _COMMON_H
 
 #include <iostream>
+#include <SDL.h>
+
+FILE _iob[] = { *stdin, *stdout, *stderr };
+
+extern "C" FILE * __cdecl __iob_func(void)
+{
+	return _iob;
+}
 
 #endif
