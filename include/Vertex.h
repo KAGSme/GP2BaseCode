25d8dc54732a4be5c827c1eb76045a2c119320c9
Kieran <kags1995@hotmail.co.uk>
2015-10-04 23:35:21 +0100

    ok

---
+++ include/Vertex.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef Vertex_h
+#define Vertex_h
+
+struct Vertex
+{
+	float x, y, z;
+	float r, g, b, a;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,44 @@
 #include "Common.h"
 #include "Graphics.h"
+#include "Vertex.h"
 
-float verts[] = { 0.0f, 1.0f, 0.0f, //Top
--1.0f, -1.0f, 0.0f, //bottom Left
-1.0f, -1.0f, 0.0f //bottome right
+Vertex verts[] = {
+	//front
+	{ -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f },//Top Left
+	{ -0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 1.0f }, //bottom Left
+	{ 0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f }, //bottom right
+	{ 0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f },//Top Right
+
+	//back
+	{ -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f },//Top Left
+	{ -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f }, //bottom Left
+	{ 0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f }, //bottom right
+	{ 0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f },//Top Right
 };
 
-float vertsM[] = { 1.0f, 1.0f, 0.0f, //Top
-0.0f, -1.0f, 0.0f, //bottom Left
-2.0f, -1.0f, 0.0f //bottome right
+GLuint indices[] = {
+	//front
+	0,1,2,
+	0,3,2,
+	//left
+	4,5,1,
+	4,1,0,
+	//right
+	3,7,2,
+	7,6,2,
+	//bottom
+	1,5,2,
+	6,2,5,
+	//top
+	4,0,7,
+	0,7,3,
+	//back
+	4,5,6,
+	4,7,6
 };
 
 GLuint VBO;
+GLuint EBO;
 
 FILE _iob[] = { *stdin, *stdout, *stderr };
 
@@ -30,10 +57,15 @@ void initScene() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	//Copy Vertex Data to VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 }
 
 void cleanUp() {
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 }
 
 void render() {
@@ -43,20 +75,28 @@ void render() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	//Make the new VBO active
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	//Esatblish its 3 coordinates per vertex with zero stride(space between elements)
-	//in array and contain floating point numbers
-	glVertexPointer(3, GL_FLOAT, 0, NULL);
-	//Establish array contains vertices(not normals, colours, texture coords etc)
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//the 3 parameter is now filled out, the pipeline needs to know the size of
+	//each vertex
+	glVertexPointer(3, GL_FLOAT, sizeof(Vertex), NULL);
+	//The Last parameter states that colours start 3 floats into 
+	//each element of the array
+	glColorPointer(4, GL_FLOAT, sizeof(Vertex), (void**)(3 * sizeof(float)));
+
+	//Establish array contains vertices and colours
 	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_COLOR_ARRAY);
 	//switch to model view
 	glMatrixMode(GL_MODELVIEW);
 	//reset using identity matrix
 	glLoadIdentity();
+
+	gluLookAt(0.0, 0.0, 0.6, 0.0, 0.0, -1.0f, 0.0, 1.0, 0.0);
+
 	//translate to -5 on z axis
 	glTranslatef(0.0f, 0.0f, -5.0f);
-
 	//begin drawing triangles
-	glDrawArrays(GL_TRIANGLES, 0, sizeof(verts) / (3 * sizeof(float)));
+	glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint), GL_UNSIGNED_INT, 0);
 }
 
 void update() {
